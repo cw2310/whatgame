1786a55431cc9ac54da1ff08609b82a035831eac
Jerry <mcarrickm@gmail.com>
2023-08-04 09:33:23 +0900

    for build webpack

--- start/webpack.config.js
+++ start/webpack.config.js
@@ -1,22 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    // ...
-    module: {
-      rules: [
-        {
-          test: /\.(png|jpe?g|gif)$/i,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]',
-                outputPath: 'assets/', // Change the output path as needed
-              },
-            },
-          ],
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        // Add other rules for handling different file types if necessary
-      ],
-    },
-    // ...
-  };
-  
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/', // Change the output path as needed
+            },
+          },
+        ],
+      }
+    ],
+  },
+};
